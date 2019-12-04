ed8c5cf3bc2f3f07085ced737e8307065425a34f
fatfutor <murat-bark@yandex.ru>
2019-12-04 22:27:04 +0300

    запущен первый код на ts

--- source/index.js
+++ source/index.js
@@ -1,19 +1,49 @@
-// (function( $ ){
-//
-//     $.fn.maxHeight = function() {
-//
-//         var max = 0;
-//
-//         this.each(function() {
-//             max = Math.max( max, $(this).height() );
-//         });
-//
-//         return max;
-//     };
-// })( jQuery );
-//
-// var tallest = $('div').maxHeight();
 "use strict";
 require('./main.scss');
-console.log('-esf-wad-');
+var Coloring;
+(function (Coloring) {
+    var GreenifyOptions = (function () {
+        function GreenifyOptions(color, backgroundColor) {
+            this.color = color;
+            this.backgroundColor = backgroundColor;
+        }
+        return GreenifyOptions;
+    }());
+    Coloring.GreenifyOptions = GreenifyOptions;
+    var Greenify = (function () {
+        function Greenify(element, options) {
+            this.element = element;
+            this.options = options;
+            this.OnCreate();
+        }
+        Greenify.prototype.OnCreate = function () {
+            this.element.css('color', this.options.color).css('background-color', this.options.backgroundColor);
+        };
+        return Greenify;
+    }());
+    Coloring.Greenify = Greenify;
+})(Coloring || (Coloring = {}));
+//jquery plugin wrapper.
+;
+(function (w, $) {
+    if (!$)
+        return false;
+    $.fn.extend({
+        Coloring: function (opts) {
+            //defaults
+            var defaults = new Coloring.GreenifyOptions('#0F0', '#000');
+            var opts = $.extend(defaults, opts);
+            return this.each(function () {
+                var o = opts;
+                var obj = $(this);
+                new Coloring.Greenify(obj, o);
+            });
+        }
+    });
+})(window, jQuery);
+$(function () {
+    var $a = $('a').Coloring();
+    var $div = $('div').Coloring({ color: '#F0F', backgroundColor: '#FFF' });
+    var $div = $('strong').Coloring({ color: '#gold', backgroundColor: 'pink' });
+});
 //# sourceMappingURL=index.js.map
--- source/index.ts
+++ source/index.ts
@@ -1,19 +1,64 @@
-
-// (function( $ ){
-//
-//     $.fn.maxHeight = function() {
-//
-//         var max = 0;
-//
-//         this.each(function() {
-//             max = Math.max( max, $(this).height() );
-//         });
-//
-//         return max;
-//     };
-// })( jQuery );
-//
-// var tallest = $('div').maxHeight();
-
 import './main.scss';
-console.log('-esf-wad-')
+
+module Coloring {
+  interface IGreenifyOptions {
+    color: string;
+    backgroundColor: string;
+  }
+  
+  export class GreenifyOptions implements IGreenifyOptions {
+    // Fields
+    color: string;
+    backgroundColor: string;
+
+    constructor(color: string, backgroundColor: string) {
+      this.color = color;
+      this.backgroundColor = backgroundColor;
+    }
+  }
+  
+  export class Greenify {
+    // Fields
+    element: JQuery;
+    options: GreenifyOptions;
+
+    constructor(element: JQuery, options: GreenifyOptions) {
+      this.element = element;
+      this.options = options;
+
+      this.OnCreate();
+    }
+
+    OnCreate() {
+      this.element.css('color', this.options.color).css('background-color', this.options.backgroundColor);
+    }
+  }
+}
+
+
+//jquery plugin wrapper.
+;(function(w,$){
+  if(!$) return false;
+
+  $.fn.extend({
+    Coloring: function(opts){
+
+      //defaults
+      var defaults: Coloring.GreenifyOptions = new Coloring.GreenifyOptions('#0F0', '#000');
+
+      var opts = $.extend(defaults, opts);
+
+      return this.each(function(){
+        var o = opts;
+        var obj = $(this);
+        new Coloring.Greenify(obj,o);
+      });
+    }
+  });
+})(window, jQuery);
+
+$(function() {
+  var $a = $('a').Coloring();
+  var $div = $('div').Coloring({ color: '#F0F', backgroundColor: '#FFF' });
+  var $div = $('strong').Coloring({ color: '#gold', backgroundColor: 'pink' });
+});
